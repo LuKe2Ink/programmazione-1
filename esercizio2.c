73cbd215585c01e4cdb7b8b22bc63bfc93709bd3
Ro0t-set <ro0trexustom@gmail.com>
2019-10-03 10:30:54 +0200

    cimmer

---
+++ esercizio2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/* 
+Dato un rettangolo, con la base e l’altezza di misura rispettivamente b ed h,
+individuare il triangolo rettangolo circoscritto ad esso (con il vertice dell’angolo
+retto coin-cidente con un vertice del rettangolo) di
+area minima; trovare poi la relazione fra b ed h in modo
+che tale triangolo sia la metà di uno equilatero.
+*/
+
+
+int main() {
+
+
+
+}
